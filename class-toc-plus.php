1307818be30b788cb2d6fefba3fb89ea8c54d033
Michael Tran <michael@dublue.com>
2023-01-05 20:55:53 +1100

    Separate TOC_Widget from original class Some cleanup

--- class-toc-plus.php
+++ class-toc-plus.php
@@ -109,6 +109,8 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 			add_shortcode( 'sitemap_pages', [ $this, 'shortcode_sitemap_pages' ] );
 			add_shortcode( 'sitemap_categories', [ $this, 'shortcode_sitemap_categories' ] );
 			add_shortcode( 'sitemap_posts', [ $this, 'shortcode_sitemap_posts' ] );
+
+			include_once 'class-toc-widget.php';
 		}
 
 
@@ -554,10 +556,13 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 		public function init() {
 			// Add compatibility with Rank Math SEO
 			if ( class_exists( 'RankMath' ) ) {
-				add_filter( 'rank_math/researches/toc_plugins', function( $toc_plugins ) {
-					$toc_plugins['table-of-contents-plus/toc.php'] = 'Table of Contents Plus';
-					return $toc_plugins;
-				} );
+				add_filter(
+					'rank_math/researches/toc_plugins',
+					function( $toc_plugins ) {
+						$toc_plugins['table-of-contents-plus/toc.php'] = 'Table of Contents Plus';
+						return $toc_plugins;
+					}
+				);
 			}
 		}
 
@@ -630,7 +635,9 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 			$custom_links_hover_colour   = $this->hex_value( trim( $_POST['custom_links_hover_colour'] ), TOC_DEFAULT_LINKS_HOVER_COLOUR );
 			$custom_links_visited_colour = $this->hex_value( trim( $_POST['custom_links_visited_colour'] ), TOC_DEFAULT_LINKS_VISITED_COLOUR );
 
-			if ( $restrict_path = trim( $_POST['restrict_path'] ) ) {
+			$restrict_path = trim( $_POST['restrict_path'] );
+
+			if ( $restrict_path ) {
 				if ( strpos( $restrict_path, '/' ) !== 0 ) {
 					// restrict path did not start with a / so unset it
 					$restrict_path = '';
@@ -737,7 +744,9 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 			<?php
 			for ( $i = TOC_MIN_START; $i <= TOC_MAX_START; $i++ ) {
 				echo '<option value="' . $i . '"';
-				if ( $i == $this->options['start'] ) echo ' selected="selected"';
+				if ( $i === $this->options['start'] ) {
+					echo ' selected="selected"';
+				}
 				echo '>' . $i . '</option>' . "\n";
 			}
 			?>
@@ -752,7 +761,7 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 			foreach ( get_post_types() as $post_type ) {
 				// make sure the post type isn't on the exclusion list
 				if ( ! in_array( $post_type, $this->exclude_post_types ) ) {
-					echo '<input type="checkbox" value="' . $post_type . '" id="auto_insert_post_types_' . $post_type .'" name="auto_insert_post_types[]"';
+					echo '<input type="checkbox" value="' . $post_type . '" id="auto_insert_post_types_' . $post_type . '" name="auto_insert_post_types[]"';
 					if ( in_array( $post_type, $this->options['auto_insert_post_types'] ) ) echo ' checked="checked"';
 					echo ' /><label for="auto_insert_post_types_' . $post_type . '"> ' . $post_type . '</label><br />';
 				}
@@ -827,7 +836,7 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 				<option value="375px"<?php if ( '375px' == $this->options['width'] ) echo ' selected="selected"'; ?>>375px</option>
 				<option value="400px"<?php if ( '400px' == $this->options['width'] ) echo ' selected="selected"'; ?>>400px</option>
 			</optgroup>
-			<optgroup label="<?php _e( 'Relative', 'table-of-contents-plus' ) ; ?>">
+			<optgroup label="<?php _e( 'Relative', 'table-of-contents-plus' ); ?>">
 				<option value="Auto"<?php if ( 'Auto' == $this->options['width'] ) echo ' selected="selected"'; ?>><?php _e( 'Auto (default)', 'table-of-contents-plus' ); ?></option>
 				<option value="25%"<?php if ( '25%' == $this->options['width'] ) echo ' selected="selected"'; ?>>25%</option>
 				<option value="33%"<?php if ( '33%' == $this->options['width'] ) echo ' selected="selected"'; ?>>33%</option>
@@ -1630,152 +1639,6 @@ if ( ! class_exists( 'TOC_Plus' ) ) :
 	} // end class
 endif;
 
-
-
-if ( ! class_exists( 'toc_widget' ) ) :
-	class toc_widget extends WP_Widget {
-
-		public function __construct() {
-			$widget_options  = [
-				'classname'   => 'toc_widget',
-				'description' => __( 'Display the table of contents in the sidebar with this widget', 'table-of-contents-plus' ),
-			];
-			$control_options = [
-				'width'   => 250,
-				'height'  => 350,
-				'id_base' => 'toc-widget',
-			];
-			parent::__construct( 'toc-widget', 'TOC+', $widget_options, $control_options );
-		}
-
-
-		/**
-		 * Widget output to the public
-		 */
-		public function widget( $args, $instance ) {
-			global $toc_plus, $wp_query;
-
-			if ( is_null( $wp_query->post ) ) {
-				return;
-			}
-
-			$items               = '';
-			$custom_toc_position = '';
-			$find                = [];
-			$replace             = [];
-
-			$toc_options         = $toc_plus->get_options();
-			$post                = get_post( $wp_query->post->ID );
-			$custom_toc_position = strpos( $post->post_content, '[toc]' );  // at this point, shortcodes haven't run yet so we can't search for <!--TOC-->
-
-			if ( $toc_plus->is_eligible( $custom_toc_position ) ) {
-				$items = $toc_plus->extract_headings( $find, $replace, wptexturize( do_shortcode( $post->post_content ) ) );
-				$title = ( array_key_exists( 'title', $instance ) ) ? apply_filters( 'widget_title', $instance['title'] ) : '';
-				if ( false !== strpos( $title, '%PAGE_TITLE%' ) ) {
-					$title = str_replace( '%PAGE_TITLE%', get_the_title(), $title );
-				}
-				if ( false !== strpos( $title, '%PAGE_NAME%' ) ) {
-					$title = str_replace( '%PAGE_NAME%', get_the_title(), $title );
-				}
-				$hide_inline = $toc_options['show_toc_in_widget_only'];
-
-				$css_classes = '';
-				// bullets?
-				if ( $toc_options['bullet_spacing'] ) {
-					$css_classes .= ' have_bullets';
-				} else {
-					$css_classes .= ' no_bullets';
-				}
-
-				if ( $items ) {
-					// before widget (defined by themes)
-					echo $args['before_widget'];
-
-					// display the widget title if one was input (before and after titles defined by themes)
-					if ( $title ) {
-						echo $args['before_title'] . $title . $args['after_title'];
-					}
-
-					// display the list
-					echo '<ul class="toc_widget_list' . $css_classes . '">' . $items . '</ul>';
-
-					// after widget (defined by themes)
-					echo $args['after_widget'];
-				}
-			}
-		}
-
-
-		/**
-		 * Update the widget settings
-		 */
-		public function update( $new_instance, $old_instance ) {
-			global $toc_plus;
-
-			$instance = $old_instance;
-
-			// strip tags for title to remove HTML (important for text inputs)
-			$instance['title'] = strip_tags( trim( $new_instance['title'] ) );
-
-			// no need to strip tags for the following
-			//$instance['hide_inline'] = $new_instance['hide_inline'];
-			$toc_plus->set_show_toc_in_widget_only( $new_instance['hide_inline'] );
-			$toc_plus->set_show_toc_in_widget_only_post_types( (array) $new_instance['show_toc_in_widget_only_post_types'] );
-
-			return $instance;
-		}
-
-
-		/**
-		 * Displays the widget settings on the widget panel.
-		 */
-		public function form( $instance ) {
-			global $toc_plus;
-			$toc_options = $toc_plus->get_options();
-
-			$defaults = [ 'title' => $toc_options['heading_text'] ];
-			$instance = wp_parse_args( (array) $instance, $defaults );
-
-?>
-			<p>
-				<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'table-of-contents-plus' ); ?>:</label>
-				<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
-			</p>
-
-			<p>
-				<input class="checkbox" type="checkbox" <?php checked( $toc_options['show_toc_in_widget_only'], 1 ); ?> id="<?php echo $this->get_field_id( 'hide_inline' ); ?>" name="<?php echo $this->get_field_name( 'hide_inline' ); ?>" value="1" /> 
-				<label for="<?php echo $this->get_field_id( 'hide_inline' ); ?>"> <?php _e( 'Show the table of contents only in the sidebar', 'table-of-contents-plus' ); ?></label>
-			</p>
-
-			<div class="show_toc_in_widget_only_post_types" style="margin: 0 0 25px 25px; display: <?php echo ( $toc_options['show_toc_in_widget_only'] == 1 ) ? 'block' : 'none'; ?>;">
-				<p><?php _e( 'For the following content types:', 'table-of-contents-plus' ); ?></p>
-
-			<?php
-			foreach ( get_post_types() as $post_type ) {
-				// make sure the post type isn't on the exclusion list
-				if ( ! in_array( $post_type, $toc_plus->get_exclude_post_types() ) ) {
-					echo '<input type="checkbox" value="' . $post_type . '" id="' . $this->get_field_id( 'show_toc_in_widget_only_post_types_' . $post_type ) . '" name="' . $this->get_field_name( 'show_toc_in_widget_only_post_types' ) . '[]"';
-					if ( in_array( $post_type, $toc_options['show_toc_in_widget_only_post_types'] ) ) {
-						echo ' checked="checked"';
-					}
-					echo ' /><label for="' . $this->get_field_id( 'show_toc_in_widget_only_post_types_' . $post_type ) . '"> ' . $post_type . '</label><br />';
-				}
-			}
-
-			?></div>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$('#<?php echo $this->get_field_id( 'hide_inline' ); ?>').click(function() {
-		$(this).parent().siblings('div.show_toc_in_widget_only_post_types').toggle('fast');
-	});
-});
-</script>
-<?php
-		}
-
-	} // end class
-endif;
 
 
 /**
