fa1de0ceb53d111481d95a898f00a6202c96b068
Maciej Maćkowiak <maciej.mackowiak@gmail.com>
2023-10-19 16:38:02 +0200

    use __DIR__ when requiring the files

--- includes/init.php
+++ includes/init.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'globals.php';
-require_once 'class-toc-plus.php';
-require_once 'class-toc-widget.php';
-require_once 'functions.php';
+require_once __DIR__ . '/globals.php';
+require_once __DIR__ . '/class-toc-plus.php';
+require_once __DIR__ . '/class-toc-widget.php';
+require_once __DIR__ . '/functions.php';
 
 // do the magic
 $toc_plus = new TOC_Plus();
